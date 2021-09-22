3683b6ee6cf94fd98f9d9156f645e5a9408e0915
Mohamed <mohamed.a.salah11@gmail.com>
2021-09-22 17:23:17 +0200

    make it better

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 export AUTH0_DOMAIN='mohamedspicer.eu.auth0.com';
 export ALGORITHMS=['RS256'];
 export API_AUDIENCE='quotation';
--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
 from models import setup_db, Person, Quote
-from auth import AuthError, requires_auth
 
 
 class QuotationTestCase(unittest.TestCase):
@@ -19,6 +18,7 @@ class QuotationTestCase(unittest.TestCase):
         self.database_path = "postgresql://{}@{}/{}".format('mohamed', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.admin_token = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFUVm1yTTA2eTFXd0h4U2hHNlZncSJ9.eyJpc3MiOiJodHRwczovL21vaGFtZWRzcGljZXIuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDYxNGIzZjg4YjM1ODNmMDA3MDVmMGFmMiIsImF1ZCI6InF1b3RhdGlvbiIsImlhdCI6MTYzMjMyMTQ3NSwiZXhwIjoxNjMyMzI4Njc1LCJhenAiOiJIY0FCdldkTHQ2TUt0WTVCQ2NLaDRFelI2NnF2WnhqdiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiY3JlYXRlOnBlcnNvbiIsImNyZWF0ZTpxdW90ZSIsImVkaXQ6cGVyc29uIiwiZWRpdDpxdW90ZSIsImdldDpwZXJzb25zIiwiZ2V0OnF1b3RlcyIsInJlbW92ZTpwZXJzb24iLCJyZW1vdmU6cXVvdGUiXX0.Pfvw4qZmKgSnbdXjTzX-DIAP_71snVDib3bvyyLARSYvPfFUcW2qd6AQ-_aw0xhR-tCGJChkH0-MFulhAFzhjTAvO4W_OaK9JpKZVYVZjkvRH0bcZBNXLiOgmGri8gC9Oj8tvHJg1Xz9mCPdk0bUsa3BTFQHS7-a8w_mMgYq6dMLNMiZjCvdNEpZW2FwXij27zaY8Ps0OeEtUiipJsj9daKKeDdam2TbtWv-VG5QAvsBy5fGrHqpKq3LOZAwxgA7B1nqAji0RgtT1vdBXj5tMHrEDatKKaILvuWClxqkc4IOBXh9XmjBSb84biYwMxlISD8cO6Qw2syIHcBTUPnFXQ"
         self.new_person = {
             'name': 'Mohamed'
         }
@@ -50,7 +50,10 @@ class QuotationTestCase(unittest.TestCase):
         pass
 
     def test_create_new_person(self):
-        res = self.client().post('/persons', json=self.new_person)
+        res = self.client().post('/persons', json=self.new_person, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -60,7 +63,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertTrue(data['total_persons'])
     
     def test_create_new_quote(self):
-        res = self.client().post('/quotes', json=self.new_quote)
+        res = self.client().post('/quotes', json=self.new_quote, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -70,7 +76,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertTrue(data['total_quotes'])
     
     def test_create_new_person_fail(self):
-        res = self.client().post('/persons', json=self.new_person_not_valid)
+        res = self.client().post('/persons', json=self.new_person_not_valid, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -79,7 +88,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Unprocessable')
     
     def test_create_new_quote_fail(self):
-        res = self.client().post('/quotes', json=self.new_quote_not_valid)
+        res = self.client().post('/quotes', json=self.new_quote_not_valid, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -88,7 +100,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Unprocessable')
     
     def test_get_persons(self):
-        res = self.client().get('/persons')
+        res = self.client().get('/persons', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -97,7 +112,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertTrue(len(data['persons']))
     
     def test_get_quotes(self):
-        res = self.client().get('/quotes')
+        res = self.client().get('/quotes', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -106,7 +124,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertTrue(data['quotes'])
     
     def test_get_persons_fail(self):
-        res = self.client().get('/personss')
+        res = self.client().get('/personss', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -115,7 +136,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'resource not found')
     
     def test_get_quotes_fail(self):
-        res = self.client().get('/quotess')
+        res = self.client().get('/quotess', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -127,7 +151,10 @@ class QuotationTestCase(unittest.TestCase):
         res = self.client().patch(
             '/persons/1',
             json={
-                'name': 'Salah'})
+                'name': 'Salah'}, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -138,7 +165,10 @@ class QuotationTestCase(unittest.TestCase):
         res = self.client().patch(
             '/quotes/1',
             json={
-                'title': 'Do somethingelse'})
+                'title': 'Do somethingelse'}, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -149,7 +179,10 @@ class QuotationTestCase(unittest.TestCase):
         res = self.client().patch(
             '/persons/99999',
             json={
-                'name': 'Salah'})
+                'name': 'Salah'}, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -161,7 +194,10 @@ class QuotationTestCase(unittest.TestCase):
         res = self.client().patch(
             '/quotes/99999',
             json={
-                'title': 'Do somethingelse'})
+                'title': 'Do somethingelse'}, headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -171,7 +207,10 @@ class QuotationTestCase(unittest.TestCase):
     
 
     def test_delete_qoute(self):
-        res = self.client().delete('/quotes/1')
+        res = self.client().delete('/quotes/1', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         quote = Quote.query.filter(Quote.id == 1).one_or_none()
@@ -182,7 +221,10 @@ class QuotationTestCase(unittest.TestCase):
         self.assertEqual(quote, None)
     
     def test_delete_person(self):
-        res = self.client().delete('/persons/1')
+        res = self.client().delete('/persons/1', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         person = Person.query.filter(Person.id == 1).one_or_none()
@@ -194,7 +236,10 @@ class QuotationTestCase(unittest.TestCase):
         
         
     def test_delete_qoute(self):
-        res = self.client().delete('/quotes/9999999')
+        res = self.client().delete('/quotes/9999999', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -203,14 +248,16 @@ class QuotationTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'resource not found')
     
     def test_delete_person(self):
-        res = self.client().delete('/persons/99999999')
+        res = self.client().delete('/persons/99999999', headers={
+            'Content-Type': 'application/json',
+            'Authorization': self.admin_token
+            })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['error'], 404)
-        self.assertEqual(data['message'], 'resource not found')
-    
+        self.assertEqual(data['message'], 'resource not found') 
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
